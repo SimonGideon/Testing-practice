e6e9b2cf9cf3413c3f7ebabd9e975cf7dafb1243
SimonGideon <simongideon918@gmail.com>
2022-11-09 01:50:52 +0300

    Solved

--- counter.js
+++ counter.js
@@ -7,4 +7,13 @@ const counter = (string) => {
     }
     
 }
-module.exports = counter;
+
+const reversed = (str='')=>{
+    let reverse = '';
+    const len = str.length;
+    for(let i=len-1; i>=0; i--){
+        reverse+= str[i];
+    }
+    return reverse;
+}
+module.exports = counter, reversed;
